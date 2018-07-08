d9458eedad717ea98a5bfc3a0430aaf6e49baad2
JQ <kamahl437@gmail.com>
2018-07-08 17:05:22 -0400

    adding physics group
    also learnin me some ts concepts

--- src/boilerplate/scenes/networkerScene.ts
+++ src/boilerplate/scenes/networkerScene.ts
@@ -12,18 +12,27 @@ export class Networker extends Phaser.Scene {
     });
   }
 
+  private cursors;
+
   preload() {
       this.load.image('cat', 'assets/networker/cat.png')
       this.load.image('background', 'assets/networker/background.png')
       this.load.image('spider', 'assets/networker/mite-alt.png')
   }
 
-  create() {
+  create():void {
       // order matters for z layering
       this.add.image(400,300,'background');
+      let spiders = this.physics.add.group();
       //scale between 0 and 1 makes the image smaller. larger than 1 is bigger
       // negative seems to reverse it.
-      this.add.image(300,400,'spider').setScale(.1);
-      this.add.image(600,400,'spider').setScale(.1);
+      spiders.create(300,400,'spider').setScale(.1);
+      spiders.create(600,400,'spider').setScale(.1);
+      this.cursors = this.input.keyboard.createCursorKeys();
+  }
+  update():void {
+      if (this.cursors.left.isDown) {
+          
+      }
   }
 }
